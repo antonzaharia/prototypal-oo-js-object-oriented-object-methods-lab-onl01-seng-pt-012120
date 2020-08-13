f73b0cb2fae569f62860aa0c23541e7e057857ca
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-08-13 19:25:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ BoardMember.prototype.veto = function(){ return "No, I must disagree"};
 BoardMember.prototype.approve = function(){ return "You can do that!"};
 BoardMember.prototype.doCharity = function(){ return "I like to help people."};
 BoardMember.prototype.releasePressStatement(){ return "You will see great things from Scuber."};
-BoardMember,prototype.sayHi() { return `Hi, my name is ${this.name}, and I was trained in ${this.training}.`}
+BoardMember,prototype.sayHi() { return `Hi, my name is ${this.name}, and I was trained in ${this.training}.`};
