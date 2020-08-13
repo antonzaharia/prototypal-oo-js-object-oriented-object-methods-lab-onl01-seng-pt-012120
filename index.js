951420f9268bd43d5db23c380c07e15366bfe36f
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-08-13 19:26:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ function BoardMember(name, homeState, training) {
 BoardMember.prototype.veto = function(){ return "No, I must disagree"};
 BoardMember.prototype.approve = function(){ return "You can do that!"};
 BoardMember.prototype.doCharity = function(){ return "I like to help people."};
-BoardMember.prototype.releasePressStatement(){ return "You will see great things from Scuber."};
+BoardMember.prototype.releasePressStatement = function(){ return "You will see great things from Scuber."};
 BoardMember.prototype.sayHi(){ return `Hi, my name is ${this.name}, and I was trained in ${this.training}.`};
