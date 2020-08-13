8e91aa76c1442a23c67985982a2e5e1ba2daeffe
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-08-13 19:22:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ function BoardMember(name, homeState, training) {
   this.training = training;
 }
 BoardMember.prototype.veto = function(){ return "No, I must disagree"};
-BoardMember.prototype.approve = function(){return "You can do that!"};
+BoardMember.prototype.approve = function(){ return "You can do that!"};
+BoardMember.prototype.doCharity = function(){ return "I like to help people."};
